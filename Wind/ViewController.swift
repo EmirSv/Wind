9069cfe755b833b3b954819ac8b77b11f0266014
Emir <seitdjelilov.emir@gmail.com>
2026-03-19 21:01:44 +0500

    Initial Commit

---
+++ Wind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wind
+//
+//  Created by Emir Seitdjelilov on 19/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
