0e2f81b99f6c62e8a3c8182bd521bcc4fbe2376e
Emir <seitdjelilov.emir@gmail.com>
2026-03-31 08:09:52 +0500

    Car photo

--- Wind/DetailsController.swift
+++ Wind/DetailsController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class DetailsController: UIViewController {
     
+    @IBOutlet weak var photo: UIImageView!
+    
     var info = ["": ""]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        photo.image = UIImage(named: "photo-1")
     }
     
     @IBAction func backPressed(_ sender: UIButton) {
